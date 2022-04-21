7725e464aecb77346292343b94a009d1cb194913
maemon <47819358+maemon4095@users.noreply.github.com>
2022-04-22 06:19:04 +0900

    parse

--- MoldSharp/MoldGenerator.Parser.cs
+++ MoldSharp/MoldGenerator.Parser.cs
@@ -1,7 +1,6 @@
-﻿using C4N.Collections.Sequence;
-using IncrementalSourceGeneratorSupplement;
+﻿using IncrementalSourceGeneratorSupplement;
 using Microsoft.CodeAnalysis;
-using StaticStateMachine;
+using Microsoft.CodeAnalysis.CSharp;
 
 namespace MoldSharp;
 
@@ -12,15 +11,22 @@ public sealed partial class MoldGenerator
         public GenerationOptions()
         {
             this.ContextVariable = "context";
-            this.ExternalContext = false;
             this.ContextSource = "this";
             this.ExportMethod = "ToString";
+            this.TransformTextMethod = "TransformText";
+            this.TransformTextMethodParams = string.Empty;
+            this.ReturnTextType = "string";
+            this.TransformTextAccessibility = "public";
         }
 
         public string ContextVariable { get; set; }
-        public bool? ExternalContext { get; set; }
+        public bool ExternalContext => this.ContextSource is null;
         public string? ContextSource { get; set; }
         public string ExportMethod { get; set; }
+        public string TransformTextMethod { get; set; }
+        public string TransformTextMethodParams { get; set; }
+        public string ReturnTextType { get; set; }
+        public string TransformTextAccessibility { get; set; }
     }
     partial struct Parser
     {
@@ -66,12 +72,14 @@ public sealed partial class MoldGenerator
         readonly string TypeDecl;
         readonly string? FullNamespace;
         readonly INamedTypeSymbol Symbol;
+        readonly GenerationOptions PreOptions;
 
         public void Parse(ReadOnlySpan<char> source)
         {
             var fullNamespace = this.FullNamespace;
             var typeDecl = this.TypeDecl;
-            var context = ProcessInitialDirective(source);
+            var context = ProcessInitialDirective(source, this.PreOptions);
+            var options = context.Options;
 
             if (fullNamespace is not null)
             {
@@ -88,14 +96,17 @@ public sealed partial class MoldGenerator
             context.Root["partial "][typeDecl].Line()
                         ['{'].Line().Indent(1);
 
-            context.TransformText["public string TransformText()"].Line()
+            context.TransformText[options.TransformTextAccessibility][' '][options.ReturnTextType][options.TransformTextMethod]['('][options.TransformTextMethodParams][')'].Line()
                                  ['{'].Line().Indent(1);
-            context.TransformText["var "][context.ContextVariable][" = this.GetTransformContext();"].Line();
+            if (!context.Options.ExternalContext)
+            {
+                context.TransformText["var "][options.ContextVariable][" = "][options.ContextSource][".GetTransformContext();"].Line();
+            }
 
             WriteTransformBody(context);
 
             context.TransformText.Line()
-                                 ["return "][context.ContextVariable]['.'][context.ExportMethod]["();"].Line().Indent(-1)
+                                 ["return "][options.ContextVariable]['.'][options.ExportMethod]["();"].Line().Indent(-1)
                                  ['}'].Line();
 
             context.Root[context.TransformText].End();
@@ -109,10 +120,10 @@ public sealed partial class MoldGenerator
             if (fullNamespace is not null) context.Root.Indent(-1)['}'].Line();
         }
 
-        static Context ProcessInitialDirective(ReadOnlySpan<char> source)
+        static Context ProcessInitialDirective(ReadOnlySpan<char> source, GenerationOptions options)
         {
-            var options = DirectiveParser.InitialParse(ref source);
-            return new(source, options);
+            var opt = DirectiveParser.InitialParse(ref source, options);
+            return new(source, opt);
         }
 
         static void WriteTransformBody(Context context)
@@ -124,10 +135,59 @@ public sealed partial class MoldGenerator
 
     struct DirectiveParser
     {
-        public static GenerationOptions InitialParse(ref ReadOnlySpan<char> source)
+        public static GenerationOptions InitialParse(ref ReadOnlySpan<char> source, GenerationOptions options)
         {
-            
+            do
+            {
+                if (!source.StartsWith("/@")) break;
+                source = source.Slice(2);
+                var key = PopKey(ref source);
+
+
+
+                var value = PopValue(ref source);
+            }
+            while (true);
+            return options;
         }
-        
+        static ReadOnlySpan<char> PopKey(ref ReadOnlySpan<char> source)
+        {
+            source = source.TrimStart();
+            for (var i = 0; i < source.Length; ++i)
+            {
+                switch (source[i])
+                {
+                    case ';' or ' ' or '\r' or '\n':
+                        var r = source.Slice(0, i);
+                        source = source.Slice(i);
+                        return r;
+                    default: break;
+                }
+            }
+
+            var result = source;
+            source = ReadOnlySpan<char>.Empty;
+            return result;
+
+        }
+        static ReadOnlySpan<char> PopValue(ref ReadOnlySpan<char> source)
+        {
+            source = source.TrimStart();
+            for (var i = 0; i < source.Length; ++i)
+            {
+                switch (source[i])
+                {
+                    case ';' or '\r' or '\n':
+                        var r = source.Slice(0, i);
+                        source = source.Slice(i);
+                        return r;
+                    default: break;
+                }
+            }
+
+            var result = source;
+            source = ReadOnlySpan<char>.Empty;
+            return result;
+        }
     }
 }
